a85885577c16790484c7eb8f812b6682578bface
Mariyamawit Alemseged <merryalem9@gmail.com>
2026-08-03 13:54:59 +0300

    named wildcard segments insetad of a bare

--- server/app.js
+++ server/app.js
@@ -58,7 +58,7 @@ app.get('/api/health', (req, res) => {
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 
-app.get('*', (req, res) => {
+app.get('/*splat', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
